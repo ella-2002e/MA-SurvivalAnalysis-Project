67197cb0509ec97b467843ee31156d1273925d97
annashaljyan <anna.shaljyan@prekog.ai>
2023-10-27 23:39:47 +0400

    Updating run.py to run the app with fastapi

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#Importing libraries
 import uvicorn
 import os
 import fastapi
@@ -5,4 +6,5 @@ import fastapi
 from survival_analysis.api import app
 
 if __name__== "__main__":
-    uvicorn.run(app, reload = True)
+    uvicorn.run(app)
+
