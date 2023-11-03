76ccebba5dec518027246d915fb7a818abcdfdd0
Anna Manasyan <anna_manasyan@edu.aua.am>
2023-11-03 14:59:25 +0400

    updated documentation for sql_interactions.py

--- survival_analysis/database_preparation/sql_interactions.py
+++ survival_analysis/database_preparation/sql_interactions.py
@@ -1,10 +1,51 @@
+"""
+Module: sql_interactions.py
+
+This module defines a Python class called 'SqlHandler' for interacting with SQLite databases. The class allows for various operations on SQLite databases, such as connecting, inserting data, retrieving data, and more.
+
+Attributes:
+- dbname (str): The name of the SQLite database.
+- table_name (str): The name of the table within the database.
+
+Methods:
+
+- __init__(self, dbname: str, table_name: str) -> None:
+    Constructor for the 'SqlHandler' class. Initializes a connection to an SQLite database and specifies the target table.
+
+- close_cnxn(self) -> None:
+    Closes the SQLite database connection.
+
+- insert_one(self) -> None:
+    Placeholder for inserting a single record into the database. (Needs implementation.)
+
+- get_table_columns(self) -> list:
+    Retrieves a list of column names for the specified table.
+
+- truncate_table(self) -> None:
+    Truncates the specified table, removing all its data.
+
+- drop_table(self) -> None:
+    Deletes the specified table from the database.
+
+- insert_many(self, df: pd.DataFrame) -> str:
+    Inserts data from a pandas DataFrame into the specified table, mapping columns to their respective database columns.
+
+- from_sql_to_pandas(self, chunksize: int, id_value: str) -> pd.DataFrame:
+    Retrieves data from the database and loads it into a pandas DataFrame in chunks of a specified size.
+
+- update_table(self, condition):
+    Placeholder for updating records in the table based on a condition. (Needs implementation.)
+
+"""
+
 import sqlite3
-import logging 
+import logging
 import pandas as pd
 import numpy as np
 import os
 from ..logger import CustomFormatter
 
+# Initialize and configure the logger
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
@@ -14,142 +55,142 @@ logger.addHandler(ch)
 
 class SqlHandler:
 
-    def __init__(self, dbname:str,table_name:str) -> None:
-        self.cnxn=sqlite3.connect(f'{dbname}.db')
-        self.cursor=self.cnxn.cursor()
-        self.dbname=dbname
-        self.table_name=table_name
+    def __init__(self, dbname: str, table_name: str) -> None:
+        """
+        Constructor for the SqlHandler class.
 
-    def close_cnxn(self)->None:
+        Parameters:
+        - dbname (str): The name of the SQLite database.
+        - table_name (str): The name of the table within the database.
+        """
+        self.cnxn = sqlite3.connect(f'{dbname}.db')
+        self.cursor = self.cnxn.cursor()
+        self.dbname = dbname
+        self.table_name = table_name
 
-        logger.info('commiting the changes')
+    def close_cnxn(self) -> None:
+        """
+        Closes the SQLite database connection.
+        """
+        logger.info('Committing the changes')
         self.cnxn.close()
-        logger.info('the connection has been closed')
+        logger.info('The connection has been closed')
 
-    def insert_one()->None:
+    def insert_one(self) -> None:
+        """
+        Placeholder for inserting a single record into the database. (Needs implementation.)
+        """
         pass
 
-    def get_table_columns(self)->list:
+    def get_table_columns(self) -> list:
+        """
+        Retrieves a list of column names for the specified table.
+
+        Returns:
+        - column_names (list): List of column names in the table.
+        """
         self.cursor.execute(f"PRAGMA table_info({self.table_name});")
         columns = self.cursor.fetchall()
-        
         column_names = [col[1] for col in columns]
-        logger.info(f'the list of columns: {column_names}')
-        # self.cursor.close()
-
+        logger.info(f'The list of columns: {column_names}')
         return column_names
     
-    def truncate_table(self)->None:
-        
-
-        query=f"DROP TABLE IF EXISTS {self.table_name};"
+    def truncate_table(self) -> None:
+        """
+        Truncates the specified table, removing all its data.
+        """
+        query = f"DROP TABLE IF EXISTS {self.table_name};"
         self.cursor.execute(query)
-        logging.info(f'the {self.table_name} is truncated')
+        logging.info(f'The {self.table_name} is truncated')
         self.cursor.close()
 
-    def drop_table(self):
-        
+    def drop_table(self) -> None:
+        """
+        Deletes the specified table from the database.
+        """
         query = f"DROP TABLE IF EXISTS {self.table_name};"
         logging.info(query)
-
         self.cursor.execute(query)
-
         self.cnxn.commit()
-
-        logging.info(f"table '{self.table_name}' deleted.")
-        logger.debug('using drop table function')
+        logging.info(f"Table '{self.table_name}' deleted.")
+        logger.debug('Using drop table function')
 
     def insert_many(self, df: pd.DataFrame) -> str:
-        
-        df=df.replace(np.nan, None) # for handling NULLS
+        """
+        Inserts data from a pandas DataFrame into the specified table, mapping columns to their respective database columns.
+
+        Parameters:
+        - df (pd.DataFrame): The DataFrame containing data to be inserted.
+
+        Returns:
+        - str: A message indicating that the data has been loaded.
+        """
+        df = df.replace(np.nan, None)  # For handling NULLS
         df.rename(columns=lambda x: x.lower(), inplace=True)
         columns = list(df.columns)
         logger.info(f'BEFORE the column intersection: {columns}')
         sql_column_names = [i.lower() for i in self.get_table_columns()]
         columns = list(set(columns) & set(sql_column_names))
         logger.info(f'AFTER the column intersection: {columns}')
-        ncolumns=list(len(columns)*'?')
-        data_to_insert=df.loc[:,columns]
-    
-        values=[tuple(i) for i in data_to_insert.values]
-        logger.info(f'the shape of the table which is going to be imported {data_to_insert.shape}')
-        # if 'geometry' in columns: #! This block is usefull in case of geometry/geography data types
-        #     df['geometry'] = df['geometry'].apply(lambda geom: dumps(geom))
-        #     ncolumns[columns.index('geometry')]= 'geography::STGeomFromText(?, 4326)'
-        
-        if len(columns)>1:
-            cols,params =', '.join(columns), ', '.join(ncolumns)
+        ncolumns = list(len(columns) * '?')
+        data_to_insert = df.loc[:, columns]
+        values = [tuple(i) for i in data_to_insert.values]
+        logger.info(f'The shape of the table which is going to be imported {data_to_insert.shape}')
+        if len(columns) > 1:
+            cols, params = ', '.join(columns), ', '.join(ncolumns)
         else:
-            cols,params =columns[0],ncolumns[0]
-
-        logger.info(f'insert structure: colnames: {cols} params: {params}')
+            cols, params = columns[0], ncolumns[0]
+        logger.info(f'Insert structure: colnames: {cols} params: {params}')
         logger.info(values[0])
-        query=f"""INSERT INTO  {self.table_name} ({cols}) VALUES ({params});"""
-        
+        query = f"""INSERT INTO {self.table_name} ({cols}) VALUES ({params});"""
         logger.info(f'QUERY: {query}')
-
         self.cursor.executemany(query, values)
         try:
             for i in self.cursor.messages:
                 logger.info(i)
         except:
             pass
-
-
         self.cnxn.commit()
-      
-        
-        logger.warning('the data is loaded')
+        logger.warning('The data is loaded')
 
-    def from_sql_to_pandas(self, chunksize:int, id_value:str) -> pd.DataFrame:
+    def from_sql_to_pandas(self, chunksize: int, id_value: str) -> pd.DataFrame:
         """
+        Retrieves data from the database and loads it into a pandas DataFrame in chunks of a specified size.
 
-        Parameters
-        ----------
-        chunksize : int
-            the chunksize for the extract
-        id_value : char
-            the values by which it should be sorted, 'MSISDN, DATE_ID'
-        dbname : char
-            (Default value = 'CVM')
-        tablename : char
-            (Default value = 'DIM_ARMENIA_MSISDNS')
-        chunksize:int :
-            
-     
-        Returns
-        -------
+        Parameters:
+        - chunksize (int): The chunksize for data extraction.
+        - id_value (str): The values by which the data should be sorted.
 
+        Returns:
+        - pd.DataFrame: A DataFrame containing the retrieved data.
         """
-        
-        offset=0
-        dfs=[]
+        offset = 0
+        dfs = []
         while True:
-            query=f"""
+            query = f"""
             SELECT * FROM {self.table_name}
                 ORDER BY {id_value} 
                 LIMIT {chunksize}
                 OFFSET {offset};
             """
-            data = pd.read_sql_query(query,self.cnxn) 
-            logger.info(f'the shape of the chunk: {data.shape}')
+            data = pd.read_sql_query(query, self.cnxn)
+            logger.info(f'The shape of the chunk: {data.shape}')
             dfs.append(data)
             offset += chunksize
             if len(dfs[-1]) < chunksize:
-                logger.warning('loading the data from SQL is finished')
-                logger.debug('connection is closed')
+                logger.warning('Loading the data from SQL is finished')
+                logger.debug('Connection is closed')
                 break
         df = pd.concat(dfs)
-
         return df
 
+    def update_table(self, condition):
+        """
+        Placeholder for updating records in the table based on a condition. (Needs implementation.)
 
-    def update_table(self,condition):
-        pass
-        # TODO: complete on your own
+        Parameters:
+        - condition: The condition to apply for updating records.
+        """
+        pass  # TODO: complete on your own
 
-   
-        
-
-
-
+# End of the SqlHandler class and module
