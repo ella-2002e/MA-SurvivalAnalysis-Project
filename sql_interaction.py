34fd3b499413895906210c1741930c7696fd9d12
Anna Manasyan <anna_manasyan@edu.aua.am>
2023-11-08 22:13:48 +0400

    added use cases for insert_one, update_table

--- sql_interaction.py
+++ sql_interaction.py
@@ -1,24 +1,33 @@
 from survival_analysis.database_preparation.sql_interactions import SqlHandler
 import pandas as pd
 
-# Get Data
 
-#For Mac Users
-data=pd.read_csv(r'Raw Data/telco.csv')
+# Get Data From CSV
 
-#For Windows users
-#data=pd.read_csv(r'Raw Data\telco.csv')
+# For Mac Users
+data = pd.read_csv(r'Raw Data/telco.csv')
 
-#Insert Data to DimCustomer
-Inst=SqlHandler('sa_db', 'DimCustomer')
+# For Windows users
+# data = pd.read_csv(r'Raw Data\telco.csv')
 
+# Insert Data to DimCustomer
+Inst = SqlHandler('sa_db', 'DimCustomer')
+Inst.insert_many(data) 
+
+"""
+UNCOMMENT THE BELOW IF NEEDED
+"""
 # Inst.truncate_table() - for deleting table contents
-#Inst.get_table_columns() - for getting column names
-Inst.insert_many(data)
 
-#Close the connection
+# Inst.get_table_columns() - for getting column names
+
+#new_dict = {"Customer_ID": 1003, "Age": 67, "Gender": "Male", "Tenure": 5, "Income": 88}
+#Inst.insert_one(new_dict) - for adding a new row to thedatabase
+
+#set_dict = {"Gender": "Female", "Age": 45}
+#cond_dict = {"Customer_ID": "= 1400"}
+#Inst.update_table(set_dict, cond_dict) - for updating the database based on some condition
+
+# Close the connection
 Inst.close_cnxn()
-
-
-
 
