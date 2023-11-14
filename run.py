53a56f9aa977a2031ac034064e00e0b867dbbf05
annashaljyan <anna.shaljyan@prekog.ai>
2023-11-15 02:28:20 +0400

    Updating run.py

--- run.py
+++ run.py
@@ -8,3 +8,6 @@ from survival_analysis.api import app
 if __name__== "__main__":
     uvicorn.run(app)
 
+# if __name__ == "__main__":
+#     uvicorn.run("run:app", host="127.0.0.1", port=8000, reload=True)
+
