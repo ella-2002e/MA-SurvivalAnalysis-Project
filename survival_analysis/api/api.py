0136c56d8f7b6261111e08412336acba8d11ca4d
annashaljyan <anna.shaljyan@prekog.ai>
2023-11-15 02:29:36 +0400

    Updating api.py to include endpoints

--- survival_analysis/api/api.py
+++ survival_analysis/api/api.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 import sqlite3
 import logging
 from ..logger import CustomFormatter
+from ..database_preparation import SqlHandler
 import os
-from pydantic import BaseModel
-from typing import Any
+import pandas as pd
+from fastapi import Path
+from typing import Any, List, Union, Optional
+#from pydantic import BaseModel
 
 app = FastAPI()
     
@@ -21,124 +24,264 @@ logger.addHandler(ch)
 # logger.error("error message")
 # logger.critical("critical message")
 
-# Defining a function to open a connection to our SQLite database
-def get_db():
-    db = sqlite3.connect('sa_db.db')
-    return db
+# Creating instances of SqlHandler for each table
+dim_customer_handler = SqlHandler(dbname='sa_db', table_name='DimCustomer')
+fact_predictions_handler = SqlHandler(dbname='sa_db', table_name='FactPredictions')
+
+# Defining functions to open connections to the databases
+def get_dim_customer_db():
+    return dim_customer_handler.cnxn
+
+def get_fact_predictions_db():
+    return fact_predictions_handler.cnxn
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    # Closing the database connections on shutdown
+    dim_customer_handler.close_cnxn()
+    fact_predictions_handler.close_cnxn()
 
 @app.get("/")
 async def root():
-    return {"message": "Initializing API for Survival Analysis project with selecting data from database, inserting data and updating data "}
+    return {
+        """
+            Initializing API for Survival Analysis project with selecting data from database, inserting data, and updating data. It now contains also several endpoints that help to identify top customers with the highest churn rate, customers with the highest/lowest CLV.
+        """
+    }
 
-@app.get("/get_data/{customer_id}")
-async def get_record(customer_id: int):
-    # Open a connection to the database
-    with get_db() as db:
-        cursor = db.cursor()
-        # Executing a SQL query to fetch the data
-        cursor.execute(f"SELECT * FROM DimCustomer WHERE Customer_ID = {customer_id}")
-        record = cursor.fetchone()
 
-    if record is None:
+@app.get("/get_dim_customer/{customer_id}")
+async def get_dim_customer_record(customer_id: int):
+    handler = dim_customer_handler
+
+    # Calling the from_sql_to_pandas function to get the data
+    data = handler.from_sql_to_pandas(chunksize=1000, id_value='Customer_ID')
+
+    # Filtering the data based on the customer_id
+    record = data[data['Customer_ID'] == customer_id].to_dict(orient='records')
+
+    if not record:
         return {"error": "Record not found"}
 
-    # Converting the record to a dictionary 
-    column_names = [description[0] for description in cursor.description]
-    record_dict = dict(zip(column_names, record))
-    return record_dict
+    return record[0]
+
+@app.get("/get_fact_predictions/{customer_id}")
+async def get_fact_predictions_record(customer_id: int, pred_period: int):
+    handler = fact_predictions_handler
+
+    # Calling the from_sql_to_pandas function to get the data
+    data = handler.from_sql_to_pandas(chunksize=1000, id_value='customer_ID')
+
+    # Filtering the data based on the customer_id and pred_period if applicable
+    if pred_period is None:
+        return {"error": "Missing pred_period for FactPredictions"}
+
+    record = data[(data['customer_ID'] == customer_id) & (data['pred_period'] == pred_period)].to_dict(orient='records')
+
+    if not record:
+        return {"error": "Record not found"}
+
+    return record[0]
 
 
-# Defining a Pydantic model for the data
-class CreateCustomerRequest(BaseModel):
-    Customer_ID: int
-    region: str
-    tenure: int
-    Age: int
-    Marital_Status: str
-    Address_ID: int
-    income: int
-    Education: str
-    Retirement: str
-    gender: str
-    Voice_Included: str
-    Internet_Included: str
-    Forward_Included: str
-    service_category: str
-    churn: str
-
-# Defining a function to open a connection to the SQLite database
-def get_db():
-    db = sqlite3.connect('sa_db.db')
-    return db
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.post("/create_data")
-async def create_record(new_data: CreateCustomerRequest):
+@app.post("/create_dim_customer/{customer_id}")
+async def create_dim_customer_record(customer_id: int, new_data: Union[dict, List[dict]]):
     try:
-        # Opening a database connection using the get_db function
-        db = get_db()
-        cursor = db.cursor()
+        # Selecting the appropriate SqlHandler instance based on the table_name
+        handler = dim_customer_handler
 
-        # Defining the SQL query to insert data into the table
-        insert_query = """
-        INSERT INTO DimCustomer (
-            Customer_ID, region, tenure, Age, Marital_Status, Address_ID, income,
-            Education, Retirement, gender, Voice_Included, Internet_Included,
-            Forward_Included, service_category, churn
-        )
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """
+        # Checking if the input data is a list of dictionaries, then use insert_many
+        if isinstance(new_data, list):
+            # Ensuring that the customer_id is set for each record
+            for record in new_data:
+                record['Customer_ID'] = customer_id
+            handler.insert_many(pd.DataFrame(new_data))
+        elif isinstance(new_data, dict):
+            # Setting the customer_id for the single record
+            new_data['Customer_ID'] = customer_id
+            handler.insert_one(new_data)
+        else:
+            return {"error": "Invalid input format"}
 
-        # Executing the insert query with the data from the new_data parameter
-        cursor.execute(insert_query, (
-            new_data.Customer_ID, new_data.region, new_data.tenure,
-            new_data.Age, new_data.Marital_Status, new_data.Address_ID,
-            new_data.income, new_data.Education, new_data.Retirement,
-            new_data.gender, new_data.Voice_Included, new_data.Internet_Included,
-            new_data.Forward_Included, new_data.service_category, new_data.churn
-        ))
-
-        # Committing the transaction to save the data in the database
-        db.commit()
-
-        return {"message": "Record created successfully"}
+        return {"message": "Record(s) created successfully"}
     except Exception as e:
         logger.error(f"Failed to insert data: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Failed to insert data: {str(e)}")
-    finally:
-        # Closing the database connection in the 'finally' block
-        db.close()
 
-
-# Pydantic model to specify the update request
-class UpdateRecordRequest(BaseModel):
-    column_name: str  # The type that column gets
-    new_value: Any # As it can be both integer and str, defined as Any. Later can find better solution
-    customer_id: int  # The type that the customer_id is
-
-@app.put("/update_data")
-async def update_record(update_request: UpdateRecordRequest):
+@app.post("/create_fact_predictions/{customer_id}/{pred_period}")
+async def create_fact_predictions_record(customer_id: int, pred_period: int, new_data: Union[dict, List[dict]]):
     try:
-        # Opening a database connection using the get_db function
-        db = get_db()
-        cursor = db.cursor()
+        # Selecting the appropriate SqlHandler instance based on the table_name
+        handler = fact_predictions_handler
 
-        # Defining the SQL query to update the specified column for the given record ID
-        update_query = f"UPDATE DimCustomer SET {update_request.column_name} = ? WHERE Customer_ID = ?"
+        # Checking if the input data is a list of dictionaries, then use insert_many
+        if isinstance(new_data, list):
+            # Ensuring that the customer_id and pred_period are set for each record
+            for record in new_data:
+                record['customer_ID'] = customer_id
+                record['pred_period'] = pred_period
+            handler.insert_many(pd.DataFrame(new_data))
+        elif isinstance(new_data, dict):
+            # Setting the customer_id and pred_period for the single record
+            new_data['customer_ID'] = customer_id
+            new_data['pred_period'] = pred_period
+            handler.insert_one(new_data)
+        else:
+            return {"error": "Invalid input format"}
 
-        # Executing the update query with the new value and record ID
-        cursor.execute(update_query, (update_request.new_value, update_request.customer_id))
+        return {"message": "Record(s) created successfully"}
+    except Exception as e:
+        logger.error(f"Failed to insert data: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to insert data: {str(e)}")
 
-        # Committing the transaction to save the data in the database
-        db.commit()
+@app.put("/update_dim_customer/{customer_id}")
+async def update_dim_customer_record(customer_id: int, update_request: dict):
+    try:
+        # Selecting the appropriate SqlHandler instance based on the table_name
+        handler = dim_customer_handler
+
+        # Initializing set_dict and cond_dict
+        set_dict = {}
+        cond_dict = {}
+
+        # Ensuring required fields are present in the request for DimCustomer
+        required_fields_dim = {'column_name', 'new_value'}
+        if not required_fields_dim.issubset(update_request.keys()):
+            return {"error": "Missing required fields in the request for DimCustomer"}
+
+        # Converting request data to dictionaries for set_dict and cond_dict
+        set_dict = {update_request['column_name']: update_request['new_value']}
+        cond_dict = {'customer_id': customer_id}
+
+        # Logging the details before the update
+        logger.info(f"Before update - Table: DimCustomer, Set: {set_dict}, Condition: {cond_dict}")
+
+        # Calling the update_table method to update the record
+        handler.update_table(set_dict=set_dict, cond_dict=cond_dict)
+
+        # Logging the details after the update
+        logger.info(f"After update - Table: DimCustomer, Set: {set_dict}, Condition: {cond_dict}")
 
         return {"message": "Record updated successfully"}
     except Exception as e:
+        logger.error(f"Failed to update data: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Failed to update data: {str(e)}")
-    finally:
-        # Closing the database connection in the 'finally' block
-        db.close()
+
+@app.put("/update_fact_predictions/{customer_id}/{pred_period}")
+async def update_fact_predictions_record(customer_id: int, pred_period: int, update_request: dict):
+    try:
+        # Selecting the appropriate SqlHandler instance based on the table_name
+        handler = fact_predictions_handler
+
+        # Initializing set_dict and cond_dict
+        set_dict = {}
+        cond_dict = {}
+
+        # Ensuring required fields are present in the request for FactPredictions
+        required_fields = {'column_name', 'new_value'}
+        if not required_fields.issubset(update_request.keys()):
+            return {"error": "Missing required fields in the request for FactPredictions"}
+
+        # Converting request data to dictionaries for set_dict and cond_dict
+        set_dict = {update_request['column_name']: update_request['new_value']}
+        cond_dict = {'customer_id': customer_id, 'pred_period': pred_period}
+
+        # Logging the details before the update
+        logger.info(f"Before update - Table: FactPredictions, Set: {set_dict}, Condition: {cond_dict}")
+
+        # Calling the update_table method to update the record
+        handler.update_table(set_dict=set_dict, cond_dict=cond_dict)
+
+        # Logging the details after the update
+        logger.info(f"After update - Table: FactPredictions, Set: {set_dict}, Condition: {cond_dict}")
+
+        return {"message": "Record updated successfully"}
+    except Exception as e:
+        logger.error(f"Failed to update data: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to update data: {str(e)}")
+    
+
+## Adding endpoints for different scenarios
+
+@app.get("/get_top_10p_churn_customers")
+async def get_top_churn_customers():
+    try:
+        handler = fact_predictions_handler
+
+        # Calling the from_sql_to_pandas function to get the data for FactPredictions
+        fact_data = handler.from_sql_to_pandas(chunksize=1000, id_value='customer_ID')
+
+        # Filtering the data for month 12
+        month_12_data = fact_data[fact_data['pred_period'] == 12]
+
+        # Sorting the data by Churn_Rate in descending order
+        sorted_data = month_12_data.sort_values(by='Churn_Rate', ascending=False)
+
+        # Calculating the number of customers to select (10% of total customers)
+        total_customers = len(sorted_data)
+        top_percentage = int(0.1 * total_customers)
+
+        # Selecting the top 10% of customers with the highest churn rate
+        top_churn_customers = sorted_data.head(top_percentage)
+
+        if top_churn_customers.empty:
+            return {"message": "No customers found"}
+
+        # Merging with DimCustomer table based on customer_ID and Customer_ID
+        dim_handler = dim_customer_handler
+        dim_data = dim_handler.from_sql_to_pandas(chunksize=1000, id_value='Customer_ID')
+        
+        merged_data = pd.merge(top_churn_customers, dim_data, how='left', left_on='customer_ID', right_on='Customer_ID')
+
+        return merged_data.to_dict(orient='records')
+    except Exception as e:
+        logger.error(f"Failed to get top churn customers: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to get top churn customers: {str(e)}")
+
+
+@app.get("/get_top_clv_customers")
+async def get_top_clv_customers(top_percentage: int = Query(..., description="Percentage of customers to select (e.g., 20 for top 20%)", ge=1, le=100)):
+    return await get_clv_customers_internal(top_percentage, True)
+
+@app.get("/get_bottom_clv_customers")
+async def get_bottom_clv_customers(bottom_percentage: int = Query(..., description="Percentage of customers to select (e.g., 20 for bottom 20%)", ge=1, le=100)):
+    return await get_clv_customers_internal(bottom_percentage, False)
+
+async def get_clv_customers_internal(selected_percentage: int, is_top: bool):
+    try:
+        handler = fact_predictions_handler
+
+        # Calling the from_sql_to_pandas function to get the data for FactPredictions
+        fact_data = handler.from_sql_to_pandas(chunksize=1000, id_value='customer_ID')
+
+        # Filtering the data for month 12
+        month_12_data = fact_data[fact_data['pred_period'] == 12]
+
+        if month_12_data.empty:
+            return {"message": "No data available for pred_period = 12"}
+
+        # Sorting the month 12 data by CLV in descending order
+        sorted_data = month_12_data.sort_values(by='CLV', ascending=False)
+
+        # Calculating the number of customers to select
+        total_customers = len(sorted_data)
+        selected_count = int(selected_percentage / 100 * total_customers)
+
+        # Selecting either the top or bottom percentage of customers based on the parameter
+        selected_customers = sorted_data.head(selected_count) if is_top else sorted_data.tail(selected_count)
+
+        if selected_customers.empty:
+            return {"message": "No customers found"}
+
+        # Merging with DimCustomer table based on customer_ID and Customer_ID
+        dim_handler = dim_customer_handler
+        dim_data = dim_handler.from_sql_to_pandas(chunksize=1000, id_value='Customer_ID')
+        
+        merged_data = pd.merge(selected_customers, dim_data, how='left', left_on='customer_ID', right_on='Customer_ID')
+
+        return {"selected_customers": merged_data.to_dict(orient='records')}
+    except Exception as e:
+        logger.error(f"Failed to get CLV customers: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to get CLV customers: {str(e)}")
+
 
