46bdd2127902a674bbbdd16001138ae35b98bd99
ella-2002e <khachatryanela@gmail.com>
2023-12-13 19:00:49 +0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     author='Anna Manasyan, Anna Shaljyan, Ela Khachatryan, Sergey Tovmasyan',
     description='Survival Analysis: Customer Churn and CLV Prediction',
     name='survival_analysis',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(include=['survival_analysis', 'survival_analysis.*']),
     include_package_data=True,
     long_description=open('README.md').read(),
